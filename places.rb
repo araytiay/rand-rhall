418d9368c433f05efe3a46460bc0147bbd2dbf3d
araytiay <araytiay@users.noreply.github.com>
2015-05-11 21:10:26 +1200

    don't know what I'm doing D:

--- places.rb
+++ places.rb
@@ -5,12 +5,17 @@ class Place
   end
 
   def describe
-    puts "You are in" name
+    puts "You are in " + name
 
+  end
 
 end
 
-File.open("map.txt") do |file|
-  while line = file.gets
-    places = line.split ","
-    places[0].to_s = Place.new(places[0], places[1..-1])
+#File.open("map.txt") do |file|
+#  while line = file.gets
+#    places = line.split ","
+#    places[0].to_s = Place.new(places[0], places[1..-1])
+#  end
+
+h = Place.new("h", ["i", "g"])
+h.describe
