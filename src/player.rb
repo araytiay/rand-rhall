40a492aa7ef235ac5eeb18986fc834641c0422ee
maxeonyx <maxeonyx@gmail.com>
2015-06-29 18:05:04 +1200

    Added inventory size limit - untested

--- src/player.rb
+++ src/player.rb
@@ -10,31 +10,63 @@ class Player
     @name = name
     @curr_place = curr_place
     @inventory = inventory
+    @inventory_size = 6
   end
 
 
   # add an object to the player's inventory
   def add_to_inventory(object, silent=false)
+    # tests to see if the item will fit
+    if @inventory.length < @inventory_size
 
-    @inventory += [object]
+      # if the object fits, add it to the player's inventory and return true
+      @inventory += [object]
 
-    if silent == false
-      @gc.d_print("You toss " + object.get_name + " into your bag.")
+      if silent == false
+        @gc.d_print("You toss " + object.get_name + " into your bag.")
+      end
+
+      return true
+
+    else
+
+      # if the object does not fit, do not add it to the inventory and return false
+      if silent=false
+        @gc.d_print("You already have "+String(@inventory_size)+" objects in your inventory.")
+      end
+
+      return false
+
     end
 
   end
 
 
+  # give the player information about the current room
   def look
     @gc.d_print("You are in " + @curr_place.get_name)
   end
 
 
-  def take_object(object)
-    add_to_inventory(object)
-    @curr_place.remove_object(object)
+  # take an object from the current place
+  def take_object(object, silent=false)
+
+    # if the player successfully adds it to their inventory,
+    # then remove it from the current place
+    if add_to_inventory(object, silent)
+
+      @curr_place.remove_object(object)
+
+    else
+
+      @gc.d_print("You cannot take it.")
+
+    end
+
   end
 
+
+  # method to jump to another place
   def go_to(new_place)
     @curr_place = new_place
   end
